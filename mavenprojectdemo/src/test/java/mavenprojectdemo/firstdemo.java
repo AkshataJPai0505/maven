e353237d9adb0cf19582a18206af8d3475eb66a9
Akshata J Pai <akshatajpai1995@gmail.com>
2021-09-05 19:49:17 +0530

    First commit

---
+++ mavenprojectdemo/src/test/java/mavenprojectdemo/firstdemo.java
@@ -0,0 +1,19 @@
+package mavenprojectdemo;
+
+import org.testng.annotations.Test;
+
+public class firstdemo {
+	@Test
+	public void first()
+	{
+		System.out.println("First test case");
+		
+	}
+	@Test
+	public void Second()
+	{
+		System.out.println("Second test case");
+		
+	}
+
+}
